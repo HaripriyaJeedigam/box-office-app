012712b928620c2701c56494bbdff5b9de313039
Haripriya <haripriyaj2002@gmail.com>
2024-06-04 22:38:33 +0530

    fetch and display data from tvmaze api. create reusable function to fetch data

---
+++ src/api/tvmaze.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://api.tvmaze.com';
+const apiGet = async queryString => {
+  const response = await fetch(`${BASE_URL}${queryString}`);
+  // `https://api.tvmaze.com/search/shows?q=$(searchStr)`
+  const body = await response.json();
+  return body;
+};
+export const searchForShows = query => apiGet(`/search/shows?q=${query}`);
