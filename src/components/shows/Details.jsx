80b5f1d639bd843f559942f9ecb01a31eb2ba8cc
Haripriya <haripriyaj2002@gmail.com>
2024-06-10 18:48:05 +0530

    displayed detailed show data on the show page

---
+++ src/components/shows/Details.jsx
@@ -0,0 +1,17 @@
+
+
+const Details = (props) => {
+    const { status, premiered, network } = props;
+    return (
+        <div>
+            <p>
+                Status: {status}
+            </p>
+            <p>
+                Premiered: {premiered} {!!network  && `on ${network.name}` }
+            </p>
+        </div>
+    );
+}
+
+export default Details;
