42e8cbf15467f51f09e7523458654da5188fa02a
Haripriya <haripriyaj2002@gmail.com>
2024-06-11 10:13:06 +0530

    created useStarredShows custom hook . placed useStarredShows hook logic in a seperate file.tell user whether show is starred or not

--- src/components/shows/ShowGrid.jsx
+++ src/components/shows/ShowGrid.jsx
@@ -1,46 +1,18 @@
-
-import { useReducer,useEffect } from 'react';
 import ShowCard from './ShowCard';
+import { useStarredShows } from '../../lib/useStarredShows';
+const ShowGrid = ({ shows }) => {
 
-const usePersistedReducer=(reducer,intialState,localStorageKey)=>{
-  const[state,dispatch]=useReducer(reducer,intialState,(intial)=>{
-    const persistedValue=localStorage.getItem(localStorageKey)
+ const[starredShows,dispatchStarred] =useStarredShows()
+  const onStarMeClick = showId => {
+    const isStarred = starredShows.includes(showId);
 
-    return persistedValue?JSON.parse(persistedValue):intial;
-   
-
-  });
-  useEffect(()=>{
-    localStorage.setItem(localStorageKey,JSON.stringify(state));
-  },[state,localStorageKey]);
-  return[state,dispatch];
+    if (isStarred) {
+      dispatchStarred({ type: 'UNSTAR', showId });
+    } else {
+      dispatchStarred({ type: 'STAR', showId });
+    }
   };
 
-
-const starredShowsReducer=(currentStarred,action)=>{
-  switch(action.type){
-    case 'STAR':return currentStarred.concat(action.showId)
-    case 'UNSTAR':return currentStarred.filter((showId)=>showId!=action.showId)
-    default:
-    return currentStarred;
-  }
-};
-
-
-const ShowGrid = ({ shows }) => {
-  const[starredShows,dispatchStarred]=usePersistedReducer(starredShowsReducer,[],'starredShows');
-  
- console.log({starredShows});
-  const onStarMeClick=(showId)=>{
-    const isStarred=starredShows.includes(showId);
-
-    if(isStarred){
-      dispatchStarred({type:'UNSTAR',showId});
-    }else{
-      dispatchStarred({type:'STAR',showId});
-    }
-  }
-   
   return (
     <div>
       {shows.map(data => (
@@ -48,11 +20,12 @@ const ShowGrid = ({ shows }) => {
           key={data.show.id}
           id={data.show.id}
           name={data.show.name}
-          image={data.show.image ? data.show.image.medium : '/not-found-image.png'
-
+          image={
+            data.show.image ? data.show.image.medium : '/not-found-image.png'
           }
           summary={data.show.summary}
           onStarMeClick={onStarMeClick}
+          isStarred={starredShows.includes(data.show.id)}
         />
       ))}
     </div>
