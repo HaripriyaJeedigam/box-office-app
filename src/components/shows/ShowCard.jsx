3a75c10d04f11babc6fe49ad414b1583214e2b92
Haripriya <haripriyaj2002@gmail.com>
2024-06-12 09:55:54 +0530

    added fade-in animation to FlexGrid. Added scale animation to  StarBtn

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -1,26 +1,42 @@
+import { useRef } from 'react';
 import styled from 'styled-components';
-import {SearchCard,SearchImgWrapper } from '../common/SearchCard';
-import {StarIcon} from '../common/Starlcon';
-const ShowCard = ({ name, image, id, summary, onStarMeClick,isStarred }) => {
+import { SearchCard, SearchImgWrapper } from '../common/SearchCard';
+import { StarIcon } from '../common/Starlcon';
+const ShowCard = ({ name, image, id, summary, onStarMeClick, isStarred }) => {
   const summaryStripped = summary
-    ? summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '')+'...'
+    ? summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '') + '...'
     : 'No description';
+    const starBtnRef=useRef();
+    const handleStarClick = ()=>{
+      onStarMeClick(id);
+      const starBtnEl=starBtnRef.current;
+      if(!starBtnEl) return;
+      if(isStarred){
+         starBtnEl.classList.remove('animate')
+      }else{
+        starBtnEl.classList.add('animate');
+      }
+    };
   return (
     <SearchCard>
-    <SearchImgWrapper>
+      <SearchImgWrapper>
         <img src={image} alt={name} />
-        </SearchImgWrapper>
+      </SearchImgWrapper>
       <h1>{name}</h1>
       <p>{summaryStripped}</p>
       <ActionSection>
         <a href={`/show/${id}`} target="_blank" rel="noreferrer">
           Read more
         </a>
-        < StarBtn type="button" onClick={() => onStarMeClick(id)}>
-       < StarIcon active={isStarred}/>
+        <StarBtn
+        ref={starBtnRef}
+          type="button"
+          onClick={handleStarClick}
+        >
+          <StarIcon active={isStarred} />
         </StarBtn>
-        </ActionSection>
-      </SearchCard>
+      </ActionSection>
+    </SearchCard>
   );
 };
 export default ShowCard;
@@ -50,5 +66,21 @@ const StarBtn = styled.button`
   align-items: center;
   &:hover {
     cursor: pointer;
+  }
+  &.animate {
+    ${StarIcon} {
+      animation: increase 0.75s ease-in forwards;
+      @keyframes increase {
+        0% {
+          transform: scale(1);
+        }
+        50% {
+          transform: scale(3) rotate(45deg);
+        }
+        100% {
+          transform: scale(1);
+        }
+      }
+    }
   }
 `;
